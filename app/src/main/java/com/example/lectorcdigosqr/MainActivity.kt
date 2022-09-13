024682a32b190e6fb88f049f7962a8071a38b3cf
Julissa JOSEPH <julissajoseph9@gmail.com>
2022-09-13 20:11:21 -0300

    Comentarios Explicaciones

--- app/src/main/java/com/example/lectorcdigosqr/MainActivity.kt
+++ app/src/main/java/com/example/lectorcdigosqr/MainActivity.kt
@@ -20,19 +20,40 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initScanner() {
+//  ver algunas de las propiedades más interesantes
         IntentIntegrator(this).initiateScan()
         val integrator = IntentIntegrator(this)
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-        integrator.setPrompt("Sigue aprendiendo en CursoKotlin.com")
+//  setDesiredBarcodeFormats: Podemos definir el tipo de códigos que queremos poder escanear.
+//  Simplemente pon IntentIntegrator. (con el punto) y te saldrán todas las opciones disponibles.
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+//  setPrompt: Añade un mensaje en la parte inferior de la pantalla del escaner.
+        integrator.setPrompt("YAHWEH")
+//  setTorchEnabled: Recibe un booleano, si es true, se activará el flash para escanear, por defecto viene a false.
         integrator.setTorchEnabled(true)
+//  setBeepEnabled: Si lo ponemos a true sonará un pitido cuando se haga un escaneo correctamente.
         integrator.setBeepEnabled(true)
         integrator.initiateScan()
-
     }
 
+
+//    onActivityResult() nos avisa si lanzamos una nueva activity y
+//    le decimos al sistema que estamos esperando respuesta.
+//    Para ello en vez de usar startActivity como siempre habría que usar
+//    startActivityForResult que será lo que tiene la librería por dentro al iniciar el escaner.
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//    lleva un override al principio de la función, es una función del sistema que
+//    ya genera una lógica por debajo. Cuando la función se llame, traerá tres valores,
+//    pero no vamos a entrar en detalle con esto porque nuestra librería
+//    tiene una función que nos simplificará también esta parte.
+
+//  dentro de la función onActivityResult una variable que la igualaremos
+//  con la clase IntentIntegrator llamando a la función parseActivityResult()
+//  que recibirá los tres parámetros que onActivityResult nos manda.
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+//    si el valor no es nulo, significará que viene de la pantalla del escaner.
         if (result != null) {
+//  Si por el contrario result.contents es null, significará que el usuario
+            //  ha entrado en la pantalla del escaner, pero no ha escaneado nada y ha vuelto a la pantalla anterior.
             if (result.contents == null) {
                 Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
             } else {
@@ -43,17 +64,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-//    https://www.youtube.com/watch?v=Wdb5okugwmU&list=PL8ie04dqq7_OcBYDpvHrcSFVoggLi3cm_&index=21
-//    7:00
-//    https://cursokotlin.com/zxing-leer-qr-codigos-de-barras-en-kotlin/
-//
-
-//https://cursokotlin.com/curso-programacion-kotlin-android/
-
-//    https://cursokotlin.com/capitulo-22-fragments-en-kotlin/
-
-
-
-
 }
+
+
+//  Zxing es una librería que llevo años utilizando en todo tipo de aplicaciones,
+//   desde personales hasta profesionales. Esta librería nos permite de una forma
+//   muy sencilla poder leer una gran cantidad de códigos,
+//   pero nosotros nos centraremos en el código QR y el código de barras.
+
+//  ZXing Android Embedded y es básicamente una librería que contiene ZXing y
+//  por encima de eso, controla la gestión de este,
+//  como los permisos de la cámara y los ciclos de vida de la app.
+//
+//  Lo primero que haremos será añadir la librería,
+//  para ello iremos al GitHub de Zxing Android Embedded y
+//  buscaremos la dependencia y la última versión estable.
+//  la última versión estable es la 4.0.2.
+//
